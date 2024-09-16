057ac4298bcc9100028411a6deef75e7e4ef9e98
MaysaraSoliman <144054764+MaysaraSoliman@users.noreply.github.com>
2024-09-16 10:58:46 +0300

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/project-vercel",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Splits third-party libraries into a separate chunk called vendor
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000 // Increase the chunk size limit warning to 1000 kB
+  }
 })
