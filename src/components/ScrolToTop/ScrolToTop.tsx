10ec8a67fb25a0e741a108838d9f360aba3f7079
MaysaraSoliman <144054764+MaysaraSoliman@users.noreply.github.com>
2024-09-16 10:48:42 +0300

    second commit

--- src/components/ScrolToTop/ScrolToTop.tsx
+++ src/components/ScrolToTop/ScrolToTop.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
+export const ScrollToTop = ({ children }: any) => {
+  const { pathname } = useLocation();
 
- export const ScrollToTop = ({children}) => {
-    const {pathname} = useLocation();
-
-    useEffect(()=>{
-        window.scrollTo(0,0);
-    }, [pathname]);
-    return children || null
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return children || null;
 };
